b61c4f92b139900e25035e2ce773b22ea9340936
Grzegorz Dyczko <gregordyczko@gmail.com>
2024-04-06 20:09:35 +0200

    Createing and adding first queries SQL and codes in Python for meaking charts

--- Query_MSSQL.sql
+++ Query_MSSQL.sql
@@ -1,4 +1,11 @@
+-- Top 10 Consoles sold
 SELECT TOP 10 Console_Name,
     Units_sold_in_million
 FROM Console
 ORDER BY Units_sold_in_million DESC
+
+-- Top 10 Games sold
+SELECT TOP 10 Game_Name,
+    Units_m
+FROM Games
+ORDER BY Units_m DESC
